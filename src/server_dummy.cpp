dacb5f2e940444a4c97cc0989d5d0d0cae15d64a
inkyant <80181074+inkyant@users.noreply.github.com>
2024-09-06 23:06:07 -0700

    working server dummy

--- src/server_dummy.cpp
+++ src/server_dummy.cpp
@@ -8,6 +8,7 @@ int main () {
     //  Prepare our context and publisher
     zmq::context_t context;
     zmq::socket_t publisher (context, zmq::socket_type::pub);
+    printf("Server dummy publishing dummy images...\n");
     publisher.bind("tcp://*:5556");
 
     int offset = 0; // for sample data
@@ -16,20 +17,25 @@ int main () {
         DepthFrameData data;
         data.timestamp = timeMillisec();
         data.bytes_per_pixel = 2;
-        data.width = 100;
-        data.height = 100;
+        data.width = 600;
+        data.height = 400;
         data.data_size = data.bytes_per_pixel * data.width * data.height;
-        // TODO: test this server example
+        data.unitsTimesThousand = 10;
+
+        offset++;
 
         uint16_t *ptr = (uint16_t*)malloc(data.data_size);
+        if (ptr == NULL) {
+            printf("unable to allocate pointer \n");
+            continue;
+        }
 
         // Fill the array with some sample data (e.g., a gradient)
         for (int y = 0; y < data.height; ++y) {
             for (int x = 0; x < data.width; ++x) {
-                ptr[x+y] = static_cast<uint16_t>((x + y + offset) % 256);
+                ptr[y*data.width+x] = static_cast<uint16_t>((x + y + offset) % 256);
             }
         }
-        offset++;
 
         zmq::message_t messageD(serialize(data));
         publisher.send(messageD, zmq::send_flags::none);
