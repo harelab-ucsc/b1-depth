350daf947a877a66b55f2e74f8428843933573d8
inkyant <80181074+inkyant@users.noreply.github.com>
2025-06-14 21:35:20 -0700

    start on pointcloud reading with multiple cams

--- src/server.cpp
+++ src/server.cpp
@@ -10,15 +10,23 @@
 int main(int argc, char * argv[]) try
 {
 
-    // create realsense pipeline
-    rs2::pipeline p;
+    rs2::context ctx;
+    // create realsense pipelines (multiple cameras = multiple pipelines)
+    std::vector<rs2::pipeline>
 
-    rs2::config config;
+    // Start a streaming pipe per each connected device
+    // https://dev.intelrealsense.com/docs/rs-multicam
     // downward facing camera serial number: 151222079584
     //  forward facing camera serial number: 150622073349
-    config.enable_device("150622073349"); 
-    config.enable_stream(RS2_STREAM_DEPTH);
-    p.start(config);
+    for (auto&& dev : ctx.query_devices())
+    {
+        rs2::pipeline pipe(ctx);
+        rs2::config cfg;
+        cfg.enable_device(dev.get_info(RS2_CAMERA_INFO_SERIAL_NUMBER));
+        pipe.start(cfg);
+        pipelines.emplace_back(pipe);
+        std::cout << "Enabling device: " << dev.get_info(RS2_CAMERA_INFO_NAME) << std::endl;
+    }
 
 
     //  Prepare zmq context and publisher
@@ -27,54 +35,26 @@ int main(int argc, char * argv[]) try
     publisher.bind("tcp://*:5556");
 
     while (true) {
+        // process the new frames from all the connected devices
+        for (uint8_t i = 0; i < pipelines.size(); i++)
+        {
+            rs2::frameset fs;
+            if (pipelines[i].poll_for_frames(&fs))
+            {
+                rs2::depth_frame depth = fs.get_depth_frame();
 
-        // Block program until frames arrive
-        rs2::frameset frames = p.wait_for_frames();
+                // create pointcloud
+                rs2::pointcloud pc;
+                rs2::points points;
 
-        // Try to get a frame of a depth image
-        rs2::depth_frame depth = frames.get_depth_frame();
+                // get pointcloud
+                points = pc.calculate(depth);
 
-        uint32_t timestamp = static_cast<uint32_t>(depth.get_timestamp() - 1725697800000.0);
-        uint32_t data_size = depth.get_data_size();
-        uint16_t unitsTimesThousand = static_cast<uint16_t>(depth.get_units() * 1000);
-        uint16_t width = depth.get_width();
-        uint16_t height = depth.get_height();
-        uint8_t bytes_per_pixel = depth.get_bytes_per_pixel();
+                zmq::message_t messagePoints1(points.get_data(), points.get_data_size());
 
-
-
-        // printf("ts domain: %d, ", depth.get_frame_timestamp_domain());
-        // "Frame timestamp was measured in relation to the camera clock and converted to OS system clock by constantly measure the difference "
-        
-        // union {
-        //     double d;
-        //     uint64_t u;
-        // } converter;
-        // converter.d = depth.get_timestamp();
-        
-        // // Extract the lower 32 bits (mantissa is lower 52 bits ieee754)
-        // printf("ts_truc: %u  ", static_cast<uint32_t>(converter.u));
-
-        printf("ts og: %.5f, ts: %u, data size: %u, units: %f, w: %u, h: %u, bpp: %u   \r", depth.get_timestamp(), timestamp, data_size, unitsTimesThousand / 1000.0, width, height, bytes_per_pixel);
-
-        DepthFrameData data;
-
-        data.timestamp = timestamp;
-        data.data_size = data_size;
-        data.unitsTimesThousand = unitsTimesThousand;
-        data.width = width;
-        data.height = height;
-        data.bytes_per_pixel = bytes_per_pixel;
-
-        zmq::message_t messageData(serialize(data));
-        zmq::message_t messageImage(depth.get_data(), depth.get_data_size());
-
-        publisher.send(messageData, zmq::send_flags::sndmore);
-        publisher.send(messageImage, zmq::send_flags::none);
-
-        // uint32_t ts = timeMillisec();
-        // zmq::message_t m(&ts, 4);
-        // publisher.send(m, zmq::send_flags::none);
+                publisher.send(messagePoints1, i != pipelines.size()-1 ? zmq::send_flags::sndmore : zmq::send_flags::none);
+            }
+        }
     }
 
     return EXIT_SUCCESS;
